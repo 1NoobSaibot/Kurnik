03242abf02bc2cbf7f3b0180b5708e6764ccf2ae
Eugene Diulin <front.end.aaj.dzencode@gmail.com>
2021-10-27 03:47:39 +0300

    [bkend] Validation error format has been changed

--- bkend/src/main.ts
+++ bkend/src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe } from '@nestjs/common';
+import { BadRequestException, ValidationError, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -6,8 +6,25 @@ const PORT = 3456
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    exceptionFactory: (errors: ValidationError[]) => {
+      const res = []
+      for (let i = 0; i < errors.length; i++) {
+        res.push({
+          field: errors[i].property,
+          message: getFirstMessage(errors[i].constraints)
+        })
+      }
+      return new BadRequestException(res)
+    }
+  }));
   await app.listen(PORT);
   console.log(`App compiled succsessfully. Listen: http://localhost:${PORT}`)
 }
 bootstrap();
+
+function getFirstMessage (messages: Record<string, string>): string {
+  for (const key in messages) {
+    return messages[key]
+  }
+}
