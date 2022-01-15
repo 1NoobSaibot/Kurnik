f825d616b140c941442ab799994fc9a9390f4f4d
Eugene Diulin <zheka.diulin@gmail.com>
2022-01-16 01:55:55 +0300

    [bkend] Database is connected

--- bkend/src/app.module.ts
+++ bkend/src/app.module.ts
@@ -4,9 +4,25 @@ import { AppService } from './app.service';
 import { GamesModule } from './games/games.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { UsersModule } from 'src/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, RoomsModule, GamesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'altf4altctrldel',
+      database: 'kurnik',
+      entities: [],
+      // TODO: Set it FALSE in production
+      synchronize: true,
+    }),
+    UsersModule,
+    RoomsModule,
+    GamesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
